34fd6d0bad4433475688b89dbfd7572df73ab447
Nived Padikkal <97820044+nived-padikkal@users.noreply.github.com>
2026-04-23 15:29:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,        // expose to network (0.0.0.0)
-    port: 5173,        // match your deploy platform port
-    allowedHosts: "all" // allow your custom domain
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      "nived-portfolio.mhserver.dpdns.org"
+    ]
   }
 })
