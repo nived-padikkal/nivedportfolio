d9d7643a4af324d439d6968554134d49154324f5
Nived Padikkal <97820044+nived-padikkal@users.noreply.github.com>
2026-04-23 15:25:58 +0530

    Configure Vite server settings for network access

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,        // expose to network (0.0.0.0)
+    port: 5173,        // match your deploy platform port
+    allowedHosts: "all" // allow your custom domain
+  }
 })
